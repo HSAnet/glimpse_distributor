1151f1e6ba1618bb7ab3f306908cb0a053f20a31
noxan <noxan@byteweaver.net>
2014-04-25 14:18:07 +0200

    add target selection to wsgi script (close #2)

--- distributor/wsgi.py
+++ distributor/wsgi.py
@@ -8,7 +8,12 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "distributor.settings")
+
+from distributor.common.utils import select_target
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "distributor.settings.%s" % select_target())
+
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
