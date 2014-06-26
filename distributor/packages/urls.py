9a9dcc04f599e73f633acd38b09cb43921839f2a
noxan <noxan@byteweaver.net>
2014-06-26 16:09:51 +0200

    add url for version view

--- distributor/packages/urls.py
+++ distributor/packages/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
 from django.views.decorators.csrf import csrf_exempt
 
-from distributor.packages.views import PackageUploadView
+from distributor.packages.views import PackageVersionView, PackageUploadView
 
 
 urlpatterns = patterns('',
+    url(r'^version/$', PackageVersionView.as_view(), name='version'),
     url(r'^upload/$', csrf_exempt(PackageUploadView.as_view()), name='upload'),
 )
