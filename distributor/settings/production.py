f97e5cdb6823052e03378d5a157bec229a0e7cbd
elnappo <elnappo@users.noreply.github.com>
2014-10-08 15:54:31 +0200

    add db server settings to production.py

--- distributor/settings/production.py
+++ distributor/settings/production.py
@@ -1,8 +1,18 @@
 from distributor.settings.base import *
 
-
 DEBUG = False
 
 TEMPLATE_DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'glimpse',
+        'USER': 'glimpse_services',
+        'PASSWORD': "changeme",
+        'HOST': '141.82.57.240',
+        'PORT': '5432',
+    },
+}
 
 ALLOWED_HOSTS = ['distributor.measure-it.net']
